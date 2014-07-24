21929634a73d214bd02d3c0e0ea67b4c2ea32368
Zoltrix <haron.shihab88@gmail.com>
2014-07-24 04:28:46 +0200

    using tapply

---
+++ rankall.R
@@ -0,0 +1,48 @@
+rankall <- function(outcome, num = "best") {
+	## Read outcome data
+	outcomes <- read.csv("outcome-of-care-measures.csv", 
+			     colClasses = "character")
+	
+	## Check that state and outcome are valid
+	
+	#validate input
+	valid.outcomes <- c("heart attack", "heart failure", "pneumonia")
+	if(!is.element(outcome, valid.outcomes)) 
+		stop("invalid outcome")
+	
+	# Determine the col index
+	index <- if(outcome == "heart attack")
+		11
+	else if (outcome == "heart failure")
+		17
+	else    23
+	
+	# Remove NA's
+	outcomes[, index] <- as.numeric(outcomes[, index])
+	outcomes <- outcomes[!is.na(outcomes[, index]), ]
+	
+	
+	# Sort each of the outcomes for a given state and select the targeted
+	# hospital by rank
+	sorted.outcomes  <- tapply(outcomes[, index], outcomes$State, 
+	   function(x) {
+		x <- outcomes[order(x), ]
+		if (num == "best") {
+			x[1, "Hospital.Name"]
+		} else if (num == "worst") {
+			x[nrow(x), "Hospital.Name"]
+		} else {
+			if (num > nrow(x))
+				NA
+			else
+				x[num, "Hospital.Name"]
+		}
+	})
+	
+	# formulate the result into a data frame as required
+	result <- data.frame(hospital = sorted.outcomes)
+	result$hospital <- as.character(result$hospital)
+	result$state    <- rownames(result)
+	
+	result
+}
